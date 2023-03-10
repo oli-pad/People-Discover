41d6fd0cbacb6db06bb060cc22f0ceb8a87c4917
riadhinformat <85174018+riadhinformat@users.noreply.github.com>
2023-03-10 12:20:45 +0100

    fix-of launch app

--- src/navigation/PreAuthNavigator.js
+++ src/navigation/PreAuthNavigator.js
@@ -2,8 +2,9 @@
 
 // module imports below
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 // Home screens imports below
 import OpeningScreen from '../screens/onboarding/opening_screen';
@@ -18,21 +19,21 @@ import Onboarding_4 from '../screens/onboarding/onboarding_4';
 import Onboarding_5 from '../screens/onboarding/onboarding_5';
 
 
-const PreAuthStack = createStackNavigator();
+const PreAuthStack = createNativeStackNavigator();
 
 const PreAuthNavigator = props => {
     return (
-      <NavigationContainer>
-        <PreAuthStack.Navigator>
-          <PreAuthStack.Screen name="OpeningScreen" component={OpeningScreen} options={{headerShown: false}}/>
-          <PreAuthStack.Screen name="LoginPage" component={LoginPage} options={{headerShown: false}}/>
-          <PreAuthStack.Screen name="Onboarding_1" component={Onboarding_1} options={{headerShown: false}}/>
-          <PreAuthStack.Screen name="Onboarding_3" component={Onboarding_3} options={{headerShown: false}}/>
-          <PreAuthStack.Screen name="Onboarding_4" component={Onboarding_4} options={{headerShown: false}}/>
-          <PreAuthStack.Screen name="Onboarding_5" component={Onboarding_5} options={{headerShown: false}}/>
-        </PreAuthStack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+            <PreAuthStack.Navigator>
+                <PreAuthStack.Screen name="OpeningScreen" component={OpeningScreen} options={{headerShown: false}}/>
+                <PreAuthStack.Screen name="LoginPage" component={LoginPage} options={{headerShown: false}}/>
+                <PreAuthStack.Screen name="Onboarding_1" component={Onboarding_1} options={{headerShown: false}}/>
+                <PreAuthStack.Screen name="Onboarding_3" component={Onboarding_3} options={{headerShown: false}}/>
+                <PreAuthStack.Screen name="Onboarding_4" component={Onboarding_4} options={{headerShown: false}}/>
+                <PreAuthStack.Screen name="Onboarding_5" component={Onboarding_5} options={{headerShown: false}}/>
+            </PreAuthStack.Navigator>
+        </NavigationContainer>
     );
-  };
+};
 
 export default PreAuthNavigator;
